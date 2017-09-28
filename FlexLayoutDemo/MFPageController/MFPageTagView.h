9379a4d0a32ae99b3a68f7e2fde42f5c9732a3b0
mafanghua <980992720@qq.com>
2017-09-28 15:35:11 +0800

    no message

---
+++ FlexLayoutDemo/MFPageController/MFPageTagView.h
@@ -0,0 +1,13 @@
+//
+//  MFPageTagView.h
+//  FlexLayoutDemo
+//
+//  Created by EEKA on 2017/9/28.
+//  Copyright © 2017年 mafanghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFPageTagView : UIView
+
+@end
