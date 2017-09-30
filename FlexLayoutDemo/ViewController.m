6417f40f3a58849ba14a6a249e2463df0be9c842
mafanghua <980992720@qq.com>
2017-09-30 13:43:11 +0800

    no message

--- FlexLayoutDemo/ViewController.m
+++ FlexLayoutDemo/ViewController.m
@@ -23,6 +23,26 @@
     [self.view addSubview:demoView];
     
     [demoView setName:@"大大标题" title:@"小题"];
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    [self addScrollView:demoView];
+}
+
+-(void)addScrollView:(UIView *)yView
+{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(yView.frame) + 30, 300, 200)];
+    view.backgroundColor = [UIColor redColor];
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 100, CGRectGetHeight(view.frame))];
+    scrollView.contentInset = UIEdgeInsetsZero;
+    scrollView.backgroundColor = [UIColor blueColor];
+    [view addSubview:scrollView];
+    
+    UIView *itemView1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, CGRectGetHeight(view.frame))];
+    itemView1.backgroundColor = [UIColor whiteColor];
+    [scrollView addSubview:itemView1];
+    
+    [self.view addSubview:view];
 }
 
 - (void)didReceiveMemoryWarning {
