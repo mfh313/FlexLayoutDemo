f8228acd6b4e32516bc701876762052db7f3af8a
mafanghua <980992720@qq.com>
2017-10-13 14:17:38 +0800

    no message

---
+++ FlexLayoutDemo/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  FlexLayoutDemo
+//
+//  Created by EEKA on 2017/10/13.
+//  Copyright © 2017年 mafanghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ FlexLayoutDemo/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  FlexLayoutDemo
+//
+//  Created by EEKA on 2017/10/13.
+//  Copyright © 2017年 mafanghua. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
